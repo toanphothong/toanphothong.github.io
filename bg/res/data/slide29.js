e7b8ef1d6a2d548ef80395a4093c1a08986db9bb
toanphothong <87720587+toanphothong@users.noreply.github.com>
2021-07-21 02:57:53 +0700

    Up

---
+++ bg/res/data/slide29.js
@@ -0,0 +1,9 @@
+(function(){var loadHandler=window['sl_{28441934-EA2C-4EA8-BDDF-F9616BAB7DBB}'];loadHandler&&loadHandler(28, '<div id="spr0_2c1d5b9"><div id="spr1_2c1d5b9" class="kern slide"><img id="img4_2c1d5b9" src="data/img6.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_2c1d5b9" class="kern slide"><div id="spr3_2c1d5b9" style="left:186px;top:24px;"><div style="width:0px;"><span id="txt0_2c1d5b9" data-width="622.164307" style="left:66.125px;top:4.6px;">Có nhiều loại báo: báo giấy, báo điện tử, báo hình…</span></div></div><div id="spr4_2c1d5b9" style="left:10px;top:141px;"><img id="img0_2c1d5b9" src="data/img80.png" width="473" height="239" alt="Graphical user interface, website, calendar\
+\
+Description automatically generated"/></div><div id="spr5_2c1d5b9" style="left:486px;top:357px;"><img id="img1_2c1d5b9" src="data/img81.png" width="479" height="215" alt="Graphical user interface, website\
+\
+Description automatically generated"/></div><div id="spr6_2c1d5b9" style="left:9px;top:343px;"><img id="img2_2c1d5b9" src="data/img82.png" width="462" height="207" alt="A picture containing calendar\
+\
+Description automatically generated"/></div><div id="spr7_2c1d5b9" style="left:477px;top:118px;"><img id="img3_2c1d5b9" src="data/img83.png" width="483" height="283" alt="A picture containing newspaper, accessory, text, building\
+\
+Description automatically generated"/></div></div></div>', '{"s":[]}');})();
